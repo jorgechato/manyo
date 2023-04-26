625206ae8d427b7eb28b633e329c67cb16fa900f
jorgechato <hack@jorgechato.com>
2023-04-26 14:43:03 +0900

    UPDATE imports

--- src/components/cms/index.ts
+++ src/components/cms/index.ts
@@ -1,3 +1,4 @@
 export { Article } from './Article';
 export { ArticleList } from './ArticleList';
 export { Tags } from './Tags';
+export { GetArticlesMetadata, GetArticleContent } from './FetchData';
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,7 @@ export { Header, Footer } from './layout';
 export { Logo } from './logo';
 export { Project, ProjectList, Indicator, StatusPageInfo } from './status-page';
 export { Readme, Repository, RepositoryList } from './git';
-export { Article, ArticleList, Tags } from './cms';
+export { Article, ArticleList, Tags, GetArticleContent, GetArticlesMetadata } from './cms';
 
 import './styles/globals.css';
 import './styles/font.css';
