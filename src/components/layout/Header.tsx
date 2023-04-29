882453bc3fdca97aec732e0022ac0db31c0dc935
jorgechato <hack@jorgechato.com>
2023-04-29 12:03:15 +0900

    FIX text align on logo header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,7 +9,7 @@ type HeaderType = {
 
 export function Header(props: HeaderType = {title: ""}) {
     return (
-        <header className="container mx-auto p-4 mt-4 mb-12 sm:mb-24 flex items-center text-sm">
+        <header className="text-center container mx-auto p-4 mt-4 mb-12 sm:mb-24 flex items-center text-sm">
             <Logo url="/" appName={props.title} />
             <ThemeButton />
         </header>
