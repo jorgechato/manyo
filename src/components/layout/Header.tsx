5c259e3e0e4176baf5f11715c727d706e081ac7f
jorgechato <hack@jorgechato.com>
2023-05-06 07:43:28 +0900

    ADD claseName props on header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,12 +4,13 @@ import { Logo } from '../logo/Logo';
 
 type HeaderType = {
     title: string;
+    className?: string;
 };
 
 
 export function Header(props: HeaderType = {title: ""}) {
     return (
-        <header className="text-center container mx-auto p-4 mt-4 mb-12 sm:mb-24 flex items-center text-sm">
+        <header className={`text-center container mx-auto p-4 mt-4 mb-12 sm:mb-24 flex items-center text-sm ${props.className}`}>
             <Logo url="/" appName={props.title} />
             <ThemeButton />
         </header>
