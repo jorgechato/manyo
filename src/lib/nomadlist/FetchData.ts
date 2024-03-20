137fe876f323643eb6620a59b18ac3ff2c1029fc
jorgechato <hack@jorgechato.com>
2024-03-20 10:45:31 +0900

    update(nomadlist): add thumbnail overwrite

--- src/lib/nomadlist/FetchData.ts
+++ src/lib/nomadlist/FetchData.ts
@@ -1,7 +1,7 @@
 import { NomadList, Location } from "./NomadList.types";
 
 
-export function cleanURL(url: string|undefined): string {
+export function cleanURL(url: string | undefined): string {
     if (!url) {
         return "";
     }
@@ -9,18 +9,19 @@ export function cleanURL(url: string|undefined): string {
     const urlArray = url.split("https://");
 
     if (urlArray.length > 2) {
-        let image = urlArray[urlArray.length-1];
+        let image = urlArray[urlArray.length - 1];
         if (image.endsWith(".jpg?")) {
-            image = image.substring(0, image.length-1-4);
+            image = image.substring(0, image.length - 1 - 4);
         }
         return `https://nomadlist.com/cdn-cgi/image/format=auto,fit=cover,width=250,height=320/https://${image}`
     }
     return url.replace("width=100,height=100", "width=250,height=320");
 }
 
-function nomadLocationToLocation(nomadLocation: any): Location {
+function nomadLocationToLocation(nomadLocation: any, thumbnailOverwrite: any): Location {
     const city = nomadLocation.place ?? nomadLocation.city;
     const citySlug = nomadLocation.place_slug ?? nomadLocation.city_slug;
+    const thumbnail = thumbnailOverwrite[city] ?? cleanURL(nomadLocation.place_photo);
 
     return {
         city: city,
@@ -28,7 +29,7 @@ function nomadLocationToLocation(nomadLocation: any): Location {
         country: nomadLocation.country,
         countrySlug: nomadLocation.country_slug,
         countryCode: nomadLocation.country_code,
-        thumbnail: cleanURL(nomadLocation.place_photo),
+        thumbnail: thumbnail,
         latitude: nomadLocation.latitude,
         longitude: nomadLocation.longitude,
         dateStart: nomadLocation.date_start,
@@ -37,14 +38,14 @@ function nomadLocationToLocation(nomadLocation: any): Location {
     } as Location;
 }
 
-export function GetLocation(): Promise<NomadList | null> {
+export function GetLocation(thumbnailOverwrite: any): Promise<NomadList | null> {
     return fetch(`https://nomadlist.com/@${process.env.NOMADLIST_USERNAME}.json?key=${process.env.NOMADLIST_KEY}`)
         .then((res) => res.json())
         .then((data) => {
             const trips: Location[] = Array.isArray(data.trips) ?
                 data.trips
                     .filter((trip: any) => new Date(trip.date_start) > new Date())
-                    .map((trip: any) => nomadLocationToLocation(trip))
+                    .map((trip: any) => nomadLocationToLocation(trip, thumbnailOverwrite))
                     .sort((a: Location, b: Location) => new Date(a.dateStart).getTime() - new Date(b.dateStart).getTime()) :
                 [data.trips];
 
